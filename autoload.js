06a3bc06db3dbca0212fba86dbc5b149f3e85f34
Morty <matrixggt@gmail.com>
2024-06-22 18:37:49 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/mortyGGT/live2d_api/",
+			cdnPath: "https://github.com/fghrsh/live2d_api/tree/1.0.1/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
